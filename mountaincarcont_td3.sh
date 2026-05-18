69544d4098354f140004edb7c9ee1638b614d010
Akoum Mohsen <mohsen.akoum@student-cs.fr>
2026-05-19 01:02:02 +0200

    push

--- mountaincarcont_td3.sh
+++ mountaincarcont_td3.sh
@@ -3,5 +3,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$SCRIPT_DIR"
-
+pip install gymnasium
+pip install matplotlib
+pip install tqdm
+pip install pandas
 python main.py --env MountainCarContinuous-v0 --alg TD3
